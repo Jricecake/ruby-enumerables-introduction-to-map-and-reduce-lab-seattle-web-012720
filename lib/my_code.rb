7555920865dd5331ed3fc899a8c4acbaad77589c
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-23 23:17:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ source_array.map {|x| x*x}
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-source_array.reduce{|x| starting_point = starting_point + x}
+source_array.reduce{|x| x+x}
 end
 
 def reduce_to_all_true(source_array)
