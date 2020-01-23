51227bcf51746d643d35c122c1cbeda9d7b3adcb
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-23 21:50:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,12 +14,7 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-  i = 0
-  while i < source_array.length do
-  source_array[i] = source_array[i] ** 2
-  i += 1
-  end
-  source_array
+source_array.map {|x| x**x}
 end
 
 def reduce_to_total(source_array, starting_point = 0)
