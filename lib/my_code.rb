f8dfc5a9af04b6c06e5c192b7c3557a5d19abb98
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-23 21:47:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,7 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  i = 0
-  while i < source_array.length do
-  source_array[i] *= 2
-  i += 1
-  end
-  source_array
+ source_array.map {|x| x * 2}
 end
 
 def map_to_square(source_array)
